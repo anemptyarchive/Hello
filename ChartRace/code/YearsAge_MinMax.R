9e31b974b1e0c310634830bd2eb9c9eebb42f50b
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2024-08-22 23:20:54 +0900

    任意の日付のグラフを作成できるように改善

--- ChartRace/code/YearsAge_MinMax.R
+++ ChartRace/code/YearsAge_MinMax.R
@@ -229,49 +229,122 @@ m <- gganimate::animate(
 )
 
 
-# 月を指定して作図 ----------------------------------------------------------------
+# バーチャートの作成 -----------------------------------------------------------
 
-# 月(月初の日付)を指定
-date_val <- "2022-06-01"
+### データの集計 -----
 
-# 作図用のデータを抽出
-mon_rank_df <- rank_df |> 
-  dplyr::filter(date == lubridate::as_date(date_val))
+# 集計日を指定
+date_val <- "2014-01-01" |> 
+  lubridate::as_date()
+date_val <- lubridate::today()
 
-# 棒グラフを作成
-graph <- ggplot(mon_rank_df, aes(x = ranking, y = age, fill = groupID, color = groupID)) + 
-  geom_bar(stat = "identity", width = 0.9, alpha = 0.8) + # 年齢バー
-  geom_text(aes(y = 0, label = paste(" ", age, "歳")), hjust = 0, color = "white") + # 年齢ラベル
-  geom_text(aes(y = 0, label = paste(groupName, " ")), hjust = 1) + # グループ名ラベル
-  coord_flip(clip = "off", expand = FALSE) + # 軸の入れ変え
-  scale_x_reverse(breaks = 1:nrow(mon_rank_df)) + # x軸(縦軸)目盛を反転
+# 最年少・最年長を指定:(min, max)
+MinMax_flag <- "min"
+MinMax_flag <- "max"
+
+# 最小or最大年齢, 順位を集計
+rank_month_df <- group_df |> # グループ名, 結成(改名)月, 解散(改名)月
+  dplyr::mutate(
+    dissolveDate = dissolveDate |> 
+      is.na() |> 
+      dplyr::if_else(
+        true  = max(lubridate::today(), date_val), # 活動中なら現在の日付or集計日
+        false = dissolveDate
+      ) # 解散・改名・現在(集計)日
+  ) |> 
+  tibble::add_column(
+    date = date_val # 活動月
+  ) |> 
+  dplyr::filter(dplyr::between(date, left = formDate, right = dissolveDate)) |> # 活動中のグループを抽出
+  dplyr::left_join(
+    join_df |> # メンバーID, 加入日, 卒業日
+      dplyr::mutate(
+        gradDate = gradDate |> 
+          is.na() |> 
+          dplyr::if_else(
+            true  = max(lubridate::today(), date_val), # 活動中なら現在の日付or集計日
+            false = gradDate
+          )
+      ), 
+    by = "groupID", relationship = "many-to-many"
+  ) |> 
+  dplyr::filter(dplyr::between(date, left = joinDate, right = gradDate)) |> # 活動中のメンバーを抽出
+  dplyr::left_join(
+    member_df |> # 生年月日
+      dplyr::select(memberID, memberName, birthDate) |> 
+      dplyr::distinct(memberID, .keep_all = TRUE), # 重複を除去
+    by = "memberID"
+  ) |> 
+  dplyr::mutate(
+    age = lubridate::interval(start = birthDate, end = date) |> 
+      lubridate::time_length(unit = "year") |> 
+      floor() # メンバー年齢
+  ) |> 
+  dplyr::summarise(
+    age  = dplyr::if_else(
+      MinMax_flag == "min", true = min(age), false = max(age), 
+    ), # 最小・最大年齢
+    member_num = dplyr::n(), # グループメンバー数
+    .by = c(date, groupID, groupName)
+  ) |> 
+  dplyr::arrange(date, age, groupID) |> # 順位付け用
+  dplyr::mutate(
+    ranking = dplyr::row_number(-age), # 順位
+    .by = date
+  ) |> 
+  dplyr::select(date, groupID, groupName, member_num, age, ranking) |> 
+  dplyr::arrange(date, ranking) # 昇順
+rank_month_df
+
+
+### グラフの作成 -----
+
+# バーチャートを作図
+graph <- ggplot(
+  data = rank_month_df, 
+  mapping = aes(x = ranking, fill = factor(groupID), color = factor(groupID))
+) + 
+  geom_bar(
+    mapping = aes(y = age), 
+    stat = "identity", width = 0.9, alpha = 0.8
+  ) + # 年齢バー
+  geom_text(
+    mapping = aes(y = 0, label = paste0("  ", age, "歳", " (", member_num, "人)")), 
+    hjust = 0, color = "white"
+  ) + # 年齢ラベル
+  geom_text(
+    mapping = aes(y = 0, label = paste(groupName, " ")), 
+    hjust = 1
+  ) + # グループ名ラベル
+  coord_flip(clip = "off", expand = FALSE) + # 軸の入替
+  scale_x_reverse(breaks = 1:nrow(rank_month_df)) + # ランク軸を反転
   theme(
-    axis.title.y = element_blank(), # y軸のラベル
-    axis.text.y = element_blank(), # y軸の目盛ラベル
-    #panel.grid.major.x = element_line(color = "grey", size = 0.1), # x軸の主目盛線
-    panel.grid.major.y = element_blank(), # y軸の主目盛線
-    #panel.grid.minor.x = element_line(color = "grey", size = 0.1), # x軸の補助目盛線
-    panel.grid.minor.y = element_blank(), # y軸の補助目盛線
+    axis.title.y = element_blank(), # y軸ラベル
+    axis.text.y  = element_blank(), # y軸目盛ラベル
+    panel.grid.major.y = element_blank(), # y軸主目盛線
+    panel.grid.minor.y = element_blank(), # y軸補助目盛線
     panel.border = element_blank(), # グラフ領域の枠線
-    #panel.background = element_blank(), # グラフ領域の背景
-    plot.title = element_text(color = "black", face = "bold", size = 20, hjust = 0.5), # 全体のタイトル
-    plot.subtitle = element_text(color = "black", size = 15, hjust = 0.5), # 全体のサブタイトル
-    plot.margin = margin(t = 10, r = 20, b = 10, l = 120, unit = "pt"), # 全体の余白
-    legend.position = "none" # 凡例の表示位置
-  ) + # 図の体裁
+    plot.title    = element_text(color = "black", face = "bold", size = 20, hjust = 0.5), # 図タイトル
+    plot.subtitle = element_text(color = "black", size = 15, hjust = 0.5), # 図サブタイトル
+    plot.margin   = margin(t = 10, r = 20, b = 10, l = 120, unit = "pt"), # 図の余白
+    legend.position = "none" # 凡例の位置
+  ) + 
   labs(
-    title = "ハロプログループの最小年齢", 
-    #title = "ハロプログループの最大年齢", 
-    subtitle = paste0(
-      lubridate::year(date_val), "年", lubridate::month(date_val), "月1日時点"), 
+    title = ifelse(
+      MinMax_flag == "min", 
+      yes = "ハロプログループの最年少メンバーの年齢", 
+      no  = "ハロプログループの最年長メンバーの年齢"
+    ), 
+    subtitle = format(date_val, format = "%Y年%m月%d日時点"), 
     y = "年齢", 
     caption = "データ:「https://github.com/xxgentaroxx/HP_DB」"
-  ) # ラベル
+  )
 graph
 
-# 画像を保存
+# 画像を書出
 ggplot2::ggsave(
-  filename = paste0("BarChartRace/output/YearsAge_Min_", date_val, ".png"), plot = graph, 
+  filename = paste0("ChartRace/output/YearsAge_", MinMax_flag, "_", date_val, ".png"), plot = graph, 
   width = 24, height = 18, units = "cm", dpi = 100
 )
+
 
