5e377a9a99d6868cff8c2d8f9052a5fe99e09b28
Shoko <61862770+anemptyarchive@users.noreply.github.com>
2024-08-24 04:53:13 +0900

    処理の改善など

--- ChartRace/code/Exp_MinMax.R
+++ ChartRace/code/Exp_MinMax.R
@@ -1,127 +1,159 @@
 
-# グループごとの最小・最大活動月数の推移を可視化 -------------------------------------------------------------------
+# グループごとの新メンバー・古参メンバー加入月数(活動歴)の推移 -----------------
 
 # 利用パッケージ
 library(tidyverse)
-library(lubridate)
 library(gganimate)
 
-# チェック用
-library(magrittr)
+# パッケージを読込
 library(ggplot2)
 
 
 # データの読込 -----------------------------------------------------------------
 
-## YearsAge_Mean.Rを参照
+## ReadData.Rを参照
+
+# 利用データを確認
+member_df # メンバー一覧
+group_df  # グループ一覧
+join_df   # 加入・卒業日一覧
 
 
-# 期間の設定 -------------------------------------------------------------------
+# バーチャートレースの作成 -----------------------------------------------------
 
-## YearsAge_Mean.Rを参照
+### 期間の設定 -----
+
+# 集計期間を指定
+date_min <- "1997-09-01" |> 
+  lubridate::as_date() |> 
+  lubridate::floor_date(unit = "month") # 集計開始(最小)月
+date_max <- "2024-09-29" |> 
+  lubridate::as_date() |> 
+  lubridate::floor_date(unit = "month") # 集計終了(最大)月
+date_max <- lubridate::today()|> 
+  lubridate::floor_date(unit = "month") # 集計終了(最大)月
 
 
-# 演出用の処理 ------------------------------------------------------------------
+### データの集計 -----
 
-### ・グループ名の対応:(改名組の表示名に対応したい) -----
+# 最年少・最年長を指定:(min, max)
+MinMax_flag <- "min"
+MinMax_flag <- "max"
 
-## YearsAge_Mean.Rを参照
-
-
-### ・結成前月と解散月の追加:(バーの変化を強調したい) -----
-
-# 結成前月・解散月のデータを作成
-member_0_df <- group_df |> 
-  dplyr::group_by(groupID) |> # 日付の再設定用にグループ化
-  dplyr::mutate(dissolveDate = dplyr::lead(dissolveDate, n = max(dplyr::n())-1)) |> # 最後の行を1行目にズラす
-  dplyr::slice_head(n = 1) |> # 1行目を抽出
-  dplyr::ungroup() |> # グループ化を解除
+# 活動月, グループID, グループ名の対応表を作成
+group_name_df <- group_df |> 
   dplyr::mutate(
-    formDate = formDate |> 
-      lubridate::rollback() |> # 結成1か月前に変更
-      lubridate::floor_date(unit = "mon"), 
-    dissolveDate = dissolveDate |> 
-      lubridate::floor_date(unit = "mon")
-  ) |> # 月単位に切り捨て
+    date_from = formDate |> 
+      lubridate::floor_date(unit = "month"), # 結成・改名月
+    date_to   = dissolveDate |> 
+      is.na() |> 
+      dplyr::if_else(
+        true  = max(lubridate::today(), date_max), # 活動中なら現在の日付or最大月
+        false = dissolveDate
+      ) |> 
+      lubridate::floor_date(unit = "month") # 解散・改名・現在(最大)月
+  ) |> 
+  dplyr::reframe(
+    date = seq(from = date_from, to = date_to, by = "month"), # 活動月
+    .by = dplyr::everything()
+  ) |> 
+  dplyr::slice_min(formDate, n = 1, with_ties = FALSE, by = c(date, groupID)) |> # 月途中の改名なら重複するので改名前を抽出
+  dplyr::select(date, groupID, groupName, formDate, dissolveDate) |> 
+  dplyr::arrange(date, groupID) # 昇順
+group_name_df
+
+# 結成前月, 解散翌月のデータを作成:(バーの変化の強調用)
+outside_df <- group_df |> 
+  dplyr::summarise(
+    formDate     = min(formDate),     # 結成日
+    dissolveDate = max(dissolveDate), # 解散日
+    .by = groupID
+  ) |> # 改名情報を統合
+  dplyr::mutate(
+    date_pre = formDate |> 
+      lubridate::floor_date(unit = "month"), # 結成月
+    date_pre = (date_pre == formDate)|> 
+      dplyr::if_else(
+        true = date_pre |> 
+          lubridate::rollback() |> 
+          lubridate::floor_date(unit = "month"), # 結成月が月初なら結成前月
+        false = date_pre
+      ), 
+    date_post = dissolveDate |> 
+      lubridate::rollforward(roll_to_first = TRUE)  # 解散翌月
+  ) |>　
   tidyr::pivot_longer(
-    cols = c(formDate, dissolveDate), 
-    names_to = "date_type", 
+    cols      = c(date_pre, date_post), 
+    names_to  = "date_type", 
     values_to = "date"
-  ) |> # 結成前月・解散月を同じ列に変形
-  dplyr::select(date, groupID) |> # 利用する列を選択
-  dplyr::filter(!is.na(date)) |> # 現在活動中のグループの解散月を除去
+  ) |> # 月列をまとめる
+  dplyr::select(date, groupID) |> 
+  dplyr::filter(!is.na(date)) |> # 活動中なら解散月を除去
   tibble::add_column(
-    groupName = " ", 
-    exp = 0
-  ) |> # メンバー数(0人)を追加
-  dplyr::filter(date > min(date_vec), date < max(date_vec)) |> # 指定した期間内のデータを抽出
-  dplyr::arrange(date, groupID) # 昇順に並び替え
+    groupName  = " ", 
+    member_exp = 0
+  ) |> # 疑似集計データを追加
+  dplyr::filter(dplyr::between(date, left = date_min, right = date_max)) |> # 集計期間の月を抽出
+  dplyr::arrange(groupID, date) # 昇順
+outside_df
 
-
-# 集計 ----------------------------------------------------------------------
-
-# サイズを取得
-group_size  <- max(group_df[["groupID"]])
-member_size <- max(member_df[["memberID"]])
-
-# 最小活動月数or最大活動月数を集計
-rank_df <- tidyr::expand_grid(
-  date = date_vec, 
-  groupID = 1:group_size, 
-  memberID = 1:member_size
-) |> # 全ての組み合わせを作成
-  dplyr::left_join(group_name_df, by = c("date", "groupID")) |> # グループ情報を結合
-  dplyr::filter(date >= formDate, date <= dissolveDate) |> # 活動中のグループを抽出
-  dplyr::select(!c(formDate, dissolveDate)) |> # 不要な列を削除
+# 最小or最大加入月数, 順位を集計
+rank_df <- group_name_df |> # 活動月, グループ名, 結成(改名)月, 解散(改名)月
+  dplyr::filter(dplyr::between(date, left = date_min, right = date_max)) |> # 集計期間の月を抽出
   dplyr::left_join(
-    join_df |> 
+    join_df |> # メンバーID, 加入日, 卒業日
       dplyr::mutate(
-        joinDate = lubridate::floor_date(joinDate, unit = "mon"), 
-        gradDate = lubridate::floor_date(gradDate, unit = "mon")
-      ), # 月単位に切り捨て
-    by = c("groupID", "memberID")
-  ) |> # 加入メンバー情報を結合
-  dplyr::filter(date >= joinDate, date < gradDate | is.na(gradDate)) |> # 活動中のメンバーを抽出
-  dplyr::select(!c(joinDate, gradDate)) |> # 不要な列を削除
+        gradDate = gradDate |> 
+          is.na() |> 
+          dplyr::if_else(
+            true  = max(lubridate::today(), date_max), # 活動中なら現在の日付or最大月
+            false = gradDate
+          )
+      ), 
+    by = "groupID", relationship = "many-to-many"
+  ) |> 
+  dplyr::filter(dplyr::between(date, left = joinDate, right = gradDate)) |> # 活動中のメンバーを抽出
   dplyr::left_join(
-    member_df |> 
-      dplyr::distinct(memberID, .keep_all = TRUE) |> # 重複を除去
-      dplyr::mutate(HPjoinDate = lubridate::floor_date(HPjoinDate, unit  = "mon")), # 月単位に切り捨て
+    member_df |> # HP加入日
+      dplyr::select(memberID, memberName, HPjoinDate) |> 
+      dplyr::slice_min(HPjoinDate, n = 1, with_ties = FALSE, by = c(memberID)), # 重複を除去
     by = "memberID"
-  ) |> # メンバー情報を結合
-  dplyr::select(date, groupID, groupName, memberID, memberName, HPjoinDate) |> # 利用する列を選択
+  ) |> 
   dplyr::mutate(
-    exp = lubridate::interval(start = HPjoinDate, end = date) |> 
-      lubridate::time_length(unit = "mon")
-  ) |> # メンバーの活動月数を計算
-  dplyr::group_by(date, groupID) |> # 最小・最大活動月数の抽出用にグループ化
-  dplyr::slice_min(exp, n = 1, with_ties = FALSE) |> # グループの最小活動月数を抽出
-  #dplyr::slice_max(exp, n = 1, with_ties = FALSE) |> # グループの最大活動月数を抽出
-  dplyr::select(!c(memberID, memberName, HPjoinDate)) |> # 不要な列を削除
-  dplyr::bind_rows(member_0_df) |> # 結成前月・解散月を追加
-  dplyr::arrange(date, exp, groupID) |> # 順位付け用に並べ替え
-  dplyr::group_by(date) |> # 順位付け用にグループ化
+    member_exp = lubridate::interval(start = HPjoinDate, end = date) |> 
+      lubridate::time_length(unit = "month") |> 
+      floor() # メンバー加入月数
+  ) |> 
+  dplyr::summarise(
+    member_exp = dplyr::if_else(
+      MinMax_flag == "min", 
+      true  = min(member_exp, na.rm = TRUE), # 最小加入月数
+      false = max(member_exp, na.rm = TRUE), # 最大加入月数
+    ), 
+    .by = c(date, groupID, groupName)
+  ) |> 
+  dplyr::bind_rows(
+    outside_df # 結成前月, 解散翌月
+  ) |> 
+  dplyr::arrange(date, member_exp, groupID) |> # 順位付け用
   dplyr::mutate(
-    groupID = factor(groupID), 
-    year = exp %/% 12, 
-    month = round(exp %% 12, digits = 1), 
-    ranking = dplyr::row_number(-exp), 
-  ) |> # ラベル用の値と順位を追加
-  dplyr::ungroup() |> # グループ化の解除
-  dplyr::select(date, groupID, groupName, exp, year, month, ranking) |> # 利用する列を選択
-  dplyr::arrange(date, ranking) # 昇順に並べ替え
+    exp_years  = member_exp %/% 12, # グループ平均加入年数
+    exp_months = member_exp %% 12,  # グループ平均加入月数 - 平均加入年数
+    ranking    = dplyr::row_number(-member_exp), # 順位
+    .by = date
+  ) |> 
+  dplyr::select(date, groupID, groupName, member_exp, exp_years, exp_months, ranking) |> 
+  dplyr::arrange(date, ranking) # 昇順
 rank_df
 
 
-# アニメーションの作成 --------------------------------------------------------------
-
-### ・フレーム数の設定 -----
+### アニメーションの作成 -----
 
 # 遷移フレーム数を指定
-t <- 9
+t <- 8
 
 # 一時停止フレーム数を指定
-s <- 1
+s <- 2
 
 # 1秒間に表示する月数を指定:(値が大きいと意図した通りにならない)
 mps <- 3
@@ -129,121 +161,197 @@ mps <- 3
 # フレーム数を取得
 n <- length(unique(rank_df[["date"]]))
 
-
-### ・バーチャートレース -----
-
-# バーチャートレースを作成:(y軸可変)
-anim <- ggplot(rank_df, aes(x = ranking, y = exp, fill = groupID, color = groupID)) + 
-  geom_bar(stat = "identity", width = 0.9, alpha = 0.8) + # 活動月数バー
-  geom_text(aes(label = paste(" ", year, "年", month, "か月")), hjust = 0) + # 活動年数ラベル
-  geom_text(aes(y = 0, label = paste(groupName, " ")), hjust = 1) + # グループ名ラベル
-  gganimate::transition_states(states = date, transition_length = t, state_length = s, wrap = FALSE) + # フレーム
+# バーチャートレースを作図:(y軸可変)
+anim <- ggplot(
+  data = rank_df, 
+  mapping = aes(x = ranking, fill = factor(groupID), color = factor(groupID))
+) + 
+  geom_bar(
+    mapping = aes(y = member_exp), 
+    stat = "identity", width = 0.9, alpha = 0.8
+  ) + # 月数バー
+  geom_text(
+    mapping = aes(y = member_exp, label = paste(" ", exp_years, "年", round(exp_months, digits = 1), "か月")), 
+    hjust = 0
+  ) + # 月数ラベル
+  geom_text(
+    mapping = aes(y = 0, label = paste(groupName, " ")), 
+    hjust = 1
+  ) + # グループ名ラベル
+  gganimate::transition_states(states = date, transition_length = t, state_length = s, wrap = FALSE) + # フレーム切替
   gganimate::ease_aes("cubic-in-out") + # アニメーションの緩急
-  gganimate::view_follow(fixed_x = TRUE) + # 表示範囲のフィット
-  coord_flip(clip = "off", expand = FALSE) + # 軸の入れ変え
-  scale_x_reverse() + # x軸を反転
+  gganimate::view_follow(fixed_x = TRUE) + # 表示範囲の可変
+  coord_flip(clip = "off", expand = FALSE) + # 軸の入替
+  scale_x_reverse() + # ランク軸を反転
   theme(
-    axis.title.x = element_blank(), # x軸のラベル
-    axis.title.y = element_blank(), # y軸のラベル
-    axis.text.x = element_blank(), # x軸の目盛ラベル
-    axis.text.y = element_blank(), # y軸の目盛ラベル
-    axis.ticks.x = element_blank(), # x軸の目盛指示線
-    axis.ticks.y = element_blank(), # y軸の目盛指示線
-    #panel.grid.major.x = element_line(color = "grey", size = 0.1), # x軸の主目盛線
-    panel.grid.major.y = element_blank(), # y軸の主目盛線
-    #panel.grid.minor.x = element_line(color = "grey", size = 0.1), # x軸の補助目盛線
-    panel.grid.minor.y = element_blank(), # y軸の補助目盛線
-    panel.border = element_blank(), # グラフ領域の枠線
-    #panel.background = element_blank(), # グラフ領域の背景
-    plot.title = element_text(color = "black", face = "bold", size = 20, hjust = 0.5), # 全体のタイトル
-    plot.subtitle = element_text(color = "black", size = 15, hjust = 0.5), # 全体のサブタイトル
-    plot.margin = margin(t = 10, r = 100, b = 10, l = 150, unit = "pt"), # 全体の余白
-    legend.position = "none" # 凡例の表示位置
-  ) + # 図の体裁
+    axis.title = element_blank(), # 軸ラベル
+    axis.text  = element_blank(), # 軸目盛ラベル
+    axis.ticks = element_blank(), # 軸目盛指示線
+    panel.grid.major.y = element_blank(), # y軸主目盛線
+    panel.grid.minor.y = element_blank(), # y軸補助目盛線
+    panel.border       = element_blank(), # グラフ領域の枠線
+    plot.title    = element_text(color = "black", face = "bold", size = 20, hjust = 0.5), # 図タイトル
+    plot.subtitle = element_text(color = "black", size = 15, hjust = 0.5), # 図サブタイトル
+    plot.margin   = margin(t = 10, r = 90, b = 10, l = 120, unit = "pt"), # 図の余白
+    legend.position = "none" # 凡例の位置
+  ) + 
   labs(
-    title = "ハロプログループの最小活動月数の推移", 
-    #title = "ハロプログループの最大活動月数の推移", 
+    title = ifelse(
+      test = MinMax_flag == "min", 
+      yes  = "ハロプログループの最小加入年数の推移", 
+      no   = "ハロプログループの最大加入年数の推移"
+    ), 
     subtitle = paste0(
       "{lubridate::year(closest_state)}年", 
       "{stringr::str_pad(lubridate::month(closest_state), width = 2, pad = 0)}月", 
       "01日時点"
     ), 
     caption = "データ:「https://github.com/xxgentaroxx/HP_DB」"
-  ) # ラベル
+  )
 
-
-# gif画像を作成
-g <- gganimate::animate(
-  plot = anim, 
-  nframes = n*(t+s), fps = (t+s)*mps, 
-  width = 900, height = 600
-)
-g
-
-# gif画像を保存
-gganimate::anim_save(filename = "BarChartRace/output/Exp_MinMax.gif", animation = g)
-
-
-# 動画を作成と保存
+# 動画を作成
 m <- gganimate::animate(
   plot = anim, 
-  nframes = n*(t+s), fps = (t+s)*mps, 
+  nframes = (t+s)*n, fps = (t+s)*mps, 
   width = 900, height = 600, 
-  renderer = gganimate::av_renderer(file = "BarChartRace/output/Exp_MinMax.mp4")
+  renderer = gganimate::av_renderer(file = paste0("ChartRace/output/JoinExp_", MinMax_flag, ".mp4"))
 )
-
 
 warnings()
 
 
-# 月を指定して作図 ----------------------------------------------------------------
+# バーチャートの作成 -----------------------------------------------------------
 
-# 月(月初の日付)を指定
-date_val <- "2022-06-01"
+### データの集計 -----
 
-# 作図用のデータを抽出
-mon_rank_df <- rank_df |> 
-  dplyr::filter(date == lubridate::as_date(date_val)) |> # 指定した月のデータを抽出
-  dplyr::filter(groupName != " ") # 演出用のデータを除去
+# 集計日を指定
+date_val <- "2014-01-01" |> 
+  lubridate::as_date()
+date_val <- lubridate::today()
 
-# 活動年数の最大値を取得
-y_max <- max(mon_rank_df[["exp"]]) %/% 12
-y_max
+# 最年少・最年長を指定:(min, max)
+MinMax_flag <- "min"
+MinMax_flag <- "max"
 
-# 棒グラフを作成
-graph <- ggplot(mon_rank_df, aes(x = ranking, y = exp, fill = groupID, color = groupID)) + 
-  geom_bar(stat = "identity", width = 0.9, alpha = 0.8) + # 活動月数バー
-  geom_text(aes(y = 0, label = paste(" ", year, "年", month, "か月")), hjust = 0, color = "white") + # 活動年数ラベル
-  geom_text(aes(y = 0, label = paste(groupName, " ")), hjust = 1) + # グループ名ラベル
-  coord_flip(clip = "off", expand = FALSE) + # 軸の入れ変え
-  scale_x_reverse(breaks = 1:nrow(mon_rank_df)) + # x軸(縦軸)を反転
-  scale_y_continuous(breaks = seq(0, y_max, 5)*12, labels = seq(0, y_max, 5)) + # y軸(横軸)目盛
+# 最小or最大加入月数, 順位を集計
+rank_month_df <- group_df |> # グループ名, 結成(改名)月, 解散(改名)月
+  dplyr::mutate(
+    dissolveDate = dissolveDate |> 
+      is.na() |> 
+      dplyr::if_else(
+        true  = max(lubridate::today(), date_val), # 活動中なら現在の日付or集計日
+        false = dissolveDate
+      ) # 解散・改名・現在(集計)日
+  ) |> 
+  tibble::add_column(
+    date = date_val # 活動月
+  ) |> 
+  dplyr::filter(dplyr::between(date, left = formDate, right = dissolveDate)) |> # 活動中のグループを抽出
+  dplyr::left_join(
+    join_df |> # メンバーID, 加入日, 卒業日
+      dplyr::mutate(
+        gradDate = gradDate |> 
+          is.na() |> 
+          dplyr::if_else(
+            true  = max(lubridate::today(), date_val), # 活動中なら現在の日付or集計日
+            false = gradDate
+          )
+      ), 
+    by = "groupID", relationship = "many-to-many"
+  ) |> 
+  dplyr::filter(dplyr::between(date, left = joinDate, right = gradDate)) |> # 活動中のメンバーを抽出
+  dplyr::left_join(
+    member_df |> # HP加入日
+      dplyr::select(memberID, memberName, HPjoinDate) |> 
+      dplyr::slice_min(HPjoinDate, n = 1, with_ties = FALSE, by = c(memberID)), # 重複を除去
+    by = "memberID"
+  ) |> 
+  dplyr::mutate(
+    member_exp = lubridate::interval(start = HPjoinDate, end = date) |> 
+      lubridate::time_length(unit = "month") |> 
+      floor() # メンバー加入月数
+  ) |> 
+  dplyr::summarise(
+    member_exp = dplyr::if_else(
+      MinMax_flag == "min", 
+      true  = min(member_exp, na.rm = TRUE), # 最小加入月数
+      false = max(member_exp, na.rm = TRUE), # 最大加入月数
+    ), 
+    member_num = dplyr::n(), # グループメンバー数
+    .by = c(date, groupID, groupName)
+  ) |> 
+  dplyr::arrange(date, member_exp, groupID) |> # 順位付け用
+  dplyr::mutate(
+    exp_years  = member_exp %/% 12, # グループ平均加入年数
+    exp_months = member_exp %% 12,  # グループ平均加入月数 - 平均加入年数
+    ranking    = dplyr::row_number(-member_exp) # 順位
+  ) |> 
+  dplyr::select(date, groupID, groupName, member_num, member_exp, exp_years, exp_months, ranking) |> 
+  dplyr::arrange(ranking) # 昇順
+rank_month_df
+
+
+### グラフの作成 -----
+
+# 年齢の最大値を取得
+years_max <- max(rank_month_df[["member_exp"]]) %/% 12
+
+# 軸目盛の間隔を指定
+tick_val <- 5
+
+# バーチャートを作図
+graph <- ggplot(
+  data = rank_month_df, 
+  mapping = aes(x = ranking, fill = factor(groupID), color = factor(groupID))
+) + 
+  geom_bar(
+    mapping = aes(y = member_exp), 
+    stat = "identity", width = 0.9, alpha = 0.8
+  ) + # 月数バー
+  geom_text(
+    mapping = aes(
+      y = 0, 
+      label = paste0("  ", exp_years, "歳", exp_months, "か月", " (", member_num, "人)")
+    ), 
+    hjust = 0, color = "white"
+  ) + # 月数ラベル
+  geom_text(
+    mapping = aes(y = 0, label = paste(groupName, " ")), 
+    hjust = 1
+  ) + # グループ名ラベル
+  coord_flip(clip = "off", expand = FALSE) + # 軸の入替
+  scale_x_reverse(breaks = 1:nrow(rank_month_df)) + # ランク軸を反転
+  scale_y_continuous(
+    breaks = seq(from = 0, to = years_max, by = tick_val) * 12, 
+    labels = seq(from = 0, to = years_max, by = tick_val)
+  ) + # 年数軸目盛
   theme(
-    axis.title.y = element_blank(), # y軸のラベル
-    axis.text.y = element_blank(), # y軸の目盛ラベル
-    axis.ticks.x = element_blank(), # x軸の目盛指示線
-    #panel.grid.major.x = element_line(color = "grey", size = 0.1), # x軸の主目盛線
-    panel.grid.major.y = element_blank(), # y軸の主目盛線
-    panel.grid.minor.y = element_blank(), # y軸の補助目盛線
+    axis.title.y = element_blank(), # y軸ラベル
+    axis.text.y  = element_blank(), # y軸目盛ラベル
+    panel.grid.major.y = element_blank(), # y軸主目盛線
+    panel.grid.minor.y = element_blank(), # y軸補助目盛線
     panel.border = element_blank(), # グラフ領域の枠線
-    #panel.background = element_blank(), # グラフ領域の背景
-    plot.title = element_text(color = "black", face = "bold", size = 20, hjust = 0.5), # 全体のタイトル
-    plot.subtitle = element_text(color = "black", size = 15, hjust = 0.5), # 全体のサブタイトル
-    plot.margin = margin(t = 10, r = 20, b = 10, l = 120, unit = "pt"), # 全体の余白
-    legend.position = "none" # 凡例の表示位置
-  ) + # 図の体裁
+    plot.title    = element_text(color = "black", face = "bold", size = 20, hjust = 0.5), # 図タイトル
+    plot.subtitle = element_text(color = "black", size = 15, hjust = 0.5), # 図サブタイトル
+    plot.margin   = margin(t = 10, r = 20, b = 10, l = 120, unit = "pt"), # 図の余白
+    legend.position = "none" # 凡例の位置
+  ) + 
   labs(
-    title = "ハロプログループの最小活動月数", 
-    #title = "ハロプログループの最大活動月数", 
-    subtitle = paste0(lubridate::year(date_val), "年", lubridate::month(date_val), "月1日時点"), 
+    title = ifelse(
+      test = MinMax_flag == "min", 
+      yes  = "ハロプログループの最年少メンバーの年齢", 
+      no   = "ハロプログループの最年長メンバーの年齢"
+    ), 
+    subtitle = format(date_val, format = "%Y年%m月%d日時点"), 
     y = "年数", 
     caption = "データ:「https://github.com/xxgentaroxx/HP_DB」"
-  ) # ラベル
+  )
 graph
 
-# 画像を保存
+# 画像を書出
 ggplot2::ggsave(
-  filename = paste0("BarChartRace/output/Exp_MinMax_", date_val, ".png"), plot = graph, 
+  filename = paste0("ChartRace/output/JoinExp_", MinMax_flag, "_", date_val, ".png"), plot = graph, 
   width = 24, height = 18, units = "cm", dpi = 100
 )
+
 
